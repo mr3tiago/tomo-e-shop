eb1fffb383a04f7563bdf91bb4f3edb13c1f4173
takakno tomohiro <takaknotomohiro@takaknos-MacBook-Air-2.local>
2017-06-24 21:23:02 +0900

    Add payment

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,20 @@
+class PaymentsController < ApplicationController
+  def create
+    @product = Product.find params[:product_id]
+    require 'payjp'
+    Payjp.api_key = 'sk_test_c62fade9d045b54cd76d7036'
+    token = Payjp::Token.create(
+      :card => {
+        :number => params[:number],
+        :cvc => params[:cvc],
+        :exp_month => params[:exp_month],
+        :exp_year => params[:exp_year]
+      }
+    )
+    charge = Payjp::Charge.create(
+      :amount => @product.price,
+      :card => token,
+      :currency => 'jpy',
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'payments/create'
+
   devise_for :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
